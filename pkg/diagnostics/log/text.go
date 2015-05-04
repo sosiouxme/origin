41a6e0aeed52f24c8c852702300f4bf0f2596422
Luke Meyer <lmeyer@redhat.com>
2015-05-04 11:06:22 -0400

    diagnostics: cross-compile terminal detection

--- pkg/diagnostics/log/text.go
+++ pkg/diagnostics/log/text.go
@@ -3,25 +3,18 @@ package log
 import (
 	"fmt"
 	ct "github.com/daviddengcn/go-colortext"
+	"github.com/openshift/origin/pkg/cmd/util"
 	"os"
-	"runtime"
 	"strings"
-	"syscall"
-	"unsafe"
 )
 
 var ttyOutput bool = true   // usually want color; but do not output colors to non-tty
 var lastNewline bool = true // keep track of newline separation
 
 func init() {
-	if runtime.GOOS == "linux" {
-		// embed linux-specific https://godoc.org/golang.org/x/crypto/ssh/terminal#IsTerminal
-		var termios syscall.Termios
-		var ioctlReadTermios = uintptr(0x5401) // syscall.TCGETS
-		_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(int(os.Stdout.Fd())), ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-		if err != 0 { // don't want color sequences in redirected output (logs, "less", etc.)
-			ttyOutput = false
-		}
+	if !util.IsTerminal(os.Stdout) {
+		// don't want color sequences in redirected output (logs, "less", etc.)
+		ttyOutput = false
 	}
 }
 
