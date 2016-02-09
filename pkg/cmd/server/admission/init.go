5f5d522e70cd3c8d8818030682bb24afbe451b3b
Luke Meyer <lmeyer@redhat.com>
2016-02-09 10:43:46 -0500

    be more explicit about configFile var name

--- pkg/cmd/server/admission/init.go
+++ pkg/cmd/server/admission/init.go
@@ -43,16 +43,15 @@ func Validate(plugins []admission.Interface) error {
 
 // GetPluginConfigFile translates from the master plugin config to a file name containing
 // a particular plugin's config (the file may be a temp file if config is embedded)
-func GetPluginConfigFile(pluginConfig map[string]api.AdmissionPluginConfig,
-	pluginName string, defConfigFile string) (string, error) {
+func GetPluginConfigFile(pluginConfig map[string]api.AdmissionPluginConfig, pluginName string, defaultConfigFilePath string) (string, error) {
 	// Check whether a config is specified for this plugin. If not, default to the
 	// global plugin config file (if any).
 	if cfg, hasConfig := pluginConfig[pluginName]; hasConfig {
-		configFile, err := pluginconfig.GetPluginConfig(cfg)
+		configFilePath, err := pluginconfig.GetPluginConfig(cfg)
 		if err != nil {
 			return "", err
 		}
-		return configFile, nil
+		return configFilePath, nil
 	}
-	return defConfigFile, nil
+	return defaultConfigFilePath, nil
 }
