df7c42a52709341c58b3668b98e8e8153c8271ee
Luke Meyer <lmeyer@redhat.com>
2018-01-23 16:36:10 -0500

    Revert "remove premature result log code"
    This reverts commit ba47df9e8c7ccd9cf2fa28943acbfc0fd0f4814c.

--- pkg/oc/admin/diagnostics/diagnostics/cluster/app_create/result.go
+++ pkg/oc/admin/diagnostics/diagnostics/cluster/app_create/result.go
@@ -47,4 +47,26 @@ func (d *AppCreate) logResult() {
 		d.out.Warn("DCluAC039", err, fmt.Sprintf("Could not write results to output file '%s': %v", filename, err))
 		return
 	}
+
+	/*
+		// get logs from the DC (display max 20 lines)
+		logOpts := &apps.DeploymentLogOptions{SinceTime: &metav1.Time{d.result.BeginTimestamp}}
+		if logReq, err := d.Factory.LogsForObject(d.app, logOpts, time.Duration(30)*time.Second); err == nil {
+			readCloser, err := logReq.Stream()
+			if err == nil {
+				defer readCloser.Close()
+			} else {
+				d.out.Error("DCluAC052", err, fmt.Sprintf("Getting logs for dc/%s failed: %v", d.appName, err))
+			}
+		} else {
+			d.out.Error("DCluAC053", err, fmt.Sprintf("Error getting logs for deployment: %v", err))
+		}
+
+		// get list of events beginning after the start time
+		if _, err := d.KubeClient.Core().Events(d.project).List(metav1.ListOptions{ResourceVersion: d.app.ResourceVersion}); err == nil {
+			// serialize events
+		} else {
+			d.out.Error("DCluAC054", err, fmt.Sprintf("Error getting list of events: %v", err))
+		}
+	*/
 }
